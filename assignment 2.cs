cb18a74384710220c76bb5d7cf9654d033c93768
aman15eth <102879608+aman15eth@users.noreply.github.com>
2022-07-01 02:48:32 -0700

    assignment2

---
+++ assignment 2.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using circlearea;
+
+
+circ c= new circ();
+Console.WriteLine("please enter raduis");
+c.raduis = Convert.ToDouble(Console.ReadLine());
+
+
+    
+Console.WriteLine("your circle  area is" +c.Area());
+Console.WriteLine("please enter length");
+c.len = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("your square  area is" + c.area1());
+
+
+
+
+
+
+
+class circ : shape
+{
+    public double raduis { get; set; }
+    public readonly double pi = 3.14;
+
+    public override double Area()
+    {
+        return pi * raduis * raduis;
+    }
+    public double len { get; set; }
+    public override double area1()
+    {
+        return len * len;
+    }
+}
+
+
+
+
+
+    
